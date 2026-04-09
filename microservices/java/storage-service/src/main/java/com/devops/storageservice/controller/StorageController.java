0cdd8902039e102469e9b739a6d4f9656b3619e9
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2026-04-09 20:14:59 +0200

    feat: Configured GitHub Actions Test workflow

---
+++ microservices/java/storage-service/src/main/java/com/devops/storageservice/controller/StorageController.java
@@ -0,0 +1,33 @@
+package com.devops.storageservice.controller;
+
+import com.devops.storageservice.dto.FileUploadRequestDto;
+import com.devops.storageservice.dto.FileUploadResponseDto;
+import lombok.RequiredArgsConstructor;
+import com.devops.storageservice.service.StorageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/api/storage")
+@RequiredArgsConstructor
+public class StorageController {
+
+    private final StorageService storageService;
+
+    @GetMapping
+    public ResponseEntity<String> helloStorage() {
+        return ResponseEntity
+                .ok()
+                .body("Hello from storage-service v1 :)");
+    }
+
+    @PostMapping("/file")
+    ResponseEntity<FileUploadResponseDto> fileUploadRequest(@RequestBody FileUploadRequestDto fileUploadRequestDto) throws URISyntaxException {
+        FileUploadResponseDto fileUploadResponseDto = storageService.fileUploadRequest(fileUploadRequestDto);
+        return ResponseEntity
+                .created(fileUploadResponseDto.getDownloadUrl().toURI())
+                .body(fileUploadResponseDto);
+    }
+}
---
+++ microservices/java/user-service/src/main/java/com/devops/userservice/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.devops.userservice.controller;
+
+import com.devops.userservice.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import com.devops.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<String> helloUser() {
+        return ResponseEntity
+                .ok()
+                .body("Hello from user-service v1 :)");
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserDto userDto) {
+        userService.createUser(userDto.getUsername(), userDto.getEmail());
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+}
