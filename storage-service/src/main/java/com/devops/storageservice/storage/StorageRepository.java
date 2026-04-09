4d30292ef780cdd53a98bc9491a997a0f6d6083e
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2026-04-09 20:12:46 +0200

    feat: Configured GitHub Actions Test workflow

--- storage-service/src/main/java/com/devops/storageservice/storage/StorageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devops.storageservice.storage;
-
-import java.net.URL;
-
-public interface StorageRepository {
-    URL getSignedUrl(String filename);
-    URL putSignedUrl(String filename);
-}
