e08a17ef358312a5f67d0b4a4def0299674eb2e5
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2026-04-26 20:38:33 +0200

    Fix: Helm anchor

--- microservices/java/user-service/src/main/java/com/devops/userservice/controller/UserController.java
+++ microservices/java/user-service/src/main/java/com/devops/userservice/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     public ResponseEntity<String> helloUser() {
         return ResponseEntity
                 .ok()
-                .body("Hello from user-service after Istio and Argo update :D");
+                .body("Hello from user-service after Istio and ArgoCD update :D");
     }
 
     @PostMapping
