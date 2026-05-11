ef12ac36d02f12f64a11534bf5de3b5757a01828
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2026-05-11 13:47:31 +0200

    feat: Update Go app

--- microservices/go/demo-go/main.go
+++ microservices/go/demo-go/main.go
@@ -9,7 +9,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/api/demo-go", func(c *gin.Context) {
-		c.String(200, "Hello from demo-go version after CI/CD finish :)")
+		c.String(200, "11.05.2026")
 	})
 
 	r.Run(":8080")
