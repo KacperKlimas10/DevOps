db78e39fe8ec88eca83d7c5a773ea92861be57ad
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2026-05-17 13:34:56 +0200

    fix: fixed services helm values files

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -10,3 +10,15 @@ output "azure_aks_kube_config" {
   value     = module.azure_aks.kube_config
   sensitive = true
 }
+
+output "azure_gha_role_client_id" {
+  value = azurerm_user_assigned_identity.devops_gha_container_registry.client_id
+}
+
+output "azure_gha_role_tenant_id" {
+  value = azurerm_user_assigned_identity.devops_gha_container_registry.tenant_id
+}
+
+output "azure_subscribtion_id" {
+  value = var.azure_subscription_id
+}
